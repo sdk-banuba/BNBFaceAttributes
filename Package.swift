df3bfb3f4878bc0ca70ee16705e44aa378012e8b
ci <ci@banuba.com>
2024-03-28 23:12:58 +0300

    1.11.1-70-g2e0f1ab74

---
+++ Package.swift
@@ -0,0 +1,98 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let bnbPackageVersion: Version = "1.11.1-70-g2e0f1ab74"
+
+let package = Package(
+    name: "BNBFaceAttributes",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "BNBFaceAttributes",
+            targets: [
+                "BNBFaceAttributes",
+                "BNBFaceAttributes_BNBSdkCore",
+                "BNBFaceAttributes_BNBFaceTracker",
+                "BNBFaceAttributes_BNBSkin",
+                "BNBFaceAttributes_BNBHair",
+                "BNBFaceAttributes_BNBEyes"
+            ]
+        ),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/sdk-banuba/BNBSdkCore.git",
+            .exact(bnbPackageVersion)
+        ),
+        .package(
+            url: "https://github.com/sdk-banuba/BNBFaceTracker.git",
+            .exact(bnbPackageVersion)
+        ),
+        .package(
+            url: "https://github.com/sdk-banuba/BNBSkin.git",
+            .exact(bnbPackageVersion)
+        ),
+        .package(
+            url: "https://github.com/sdk-banuba/BNBHair.git",
+            .exact(bnbPackageVersion)
+        ),
+        .package(
+            url: "https://github.com/sdk-banuba/BNBEyes.git",
+            .exact(bnbPackageVersion)
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "BNBFaceAttributes",
+            url: "https://d2cm7wtcqqw29.cloudfront.net/1.11.1-70-g2e0f1ab74/BNBFaceAttributes.zip",
+            checksum: "242c47f6e913e17261197578b83afc574efc27f609b1860740fadadc89993376"
+        ),
+        .target(
+            name: "BNBFaceAttributes_BNBSdkCore",
+            dependencies: [
+                .product(
+                    name: "BNBSdkCore",
+                    package: "BNBSdkCore"
+                ),
+            ]
+        ),
+        .target(
+            name: "BNBFaceAttributes_BNBFaceTracker",
+            dependencies: [
+                .product(
+                    name: "BNBFaceTracker",
+                    package: "BNBFaceTracker"
+                ),
+            ]
+        ),
+        .target(
+            name: "BNBFaceAttributes_BNBSkin",
+            dependencies: [
+                .product(
+                    name: "BNBSkin",
+                    package: "BNBSkin"
+                ),
+            ]
+        ),
+        .target(
+            name: "BNBFaceAttributes_BNBHair",
+            dependencies: [
+                .product(
+                    name: "BNBHair",
+                    package: "BNBHair"
+                ),
+            ]
+        ),
+        .target(
+            name: "BNBFaceAttributes_BNBEyes",
+            dependencies: [
+                .product(
+                    name: "BNBEyes",
+                    package: "BNBEyes"
+                ),
+            ]
+        ),
+    ]
+)
---
+++ README.md
@@ -0,0 +1,11 @@
+[![](https://www.banuba.com/hubfs/Banuba_November2018/Images/Banuba%20SDK.png)](https://docs.banuba.com/face-ar-sdk-v1/ios/ios_overview)
+
+## Banuba Sdk BNBFaceAttributes swift package
+
+#### About packages see [HERE](https://docs.banuba.com/face-ar-sdk-v1/ios/ios_packages)
+
+#### Package version: **1.11.1-70-g2e0f1ab74**
+
+#### **[Banuba homepage](https://banuba.com)**
+
+#### **[License](https://www.banuba.com/terms)**
