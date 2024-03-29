43fd3342ece18b13a9c71da416703c599eacc2a4
Ihar Tumašyk <ihar.tumashyk@banuba.com>
2024-03-29 13:36:16 +0300

    1.11.1-70-g9084b7e01b

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let bnbPackageVersion: Version = "1.11.1-71-g826381c83"
+let bnbPackageVersion: Version = "1.11.1-70-g9084b7e01b"
 
 let package = Package(
     name: "BNBFaceAttributes",
@@ -46,8 +46,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BNBFaceAttributes",
-            url: "https://d2cm7wtcqqw29.cloudfront.net/1.11.1-71-g826381c83/BNBFaceAttributes.zip",
-            checksum: "45422774424dfcef3ff6c159387734f0ef9bc8b48b680aa6886175e192778fbd"
+            url: "None",
+            checksum: "46d6cf71df169a9b2a793f522eb4d2d617c3098472d92914ca613a30d225b772"
         ),
         .target(
             name: "BNBFaceAttributes_BNBSdkCore",
